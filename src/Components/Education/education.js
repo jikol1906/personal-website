e5d8263c114f0e5b114ff178091b4db366746228
borisgrunwald <borisgrunwald@gmail.com>
2020-07-12 14:55:08 +0200

    WIP

--- src/Components/Education/education.js
+++ src/Components/Education/education.js
@@ -1,10 +1,44 @@
 import React from 'react'
 import styles from './education.module.scss'
+import HeadingTwo from '../../Typography/HeadingTwo/headingTwo'
 
 export default function Education() {
+
+    console.log(styles)
     return (
         <section className={styles.education}>
+            <HeadingTwo otherStyles={styles.heading}>Timeline</HeadingTwo>
             <div className={styles.timeline}></div>
+            <TimelineEvent gridRow={2}>
+                <HeadingTwo>Some text</HeadingTwo>
+            </TimelineEvent>
+            <TimelineEvent right mtop={'2rem'} gridRow={2}>
+                <HeadingTwo>Some text</HeadingTwo>
+            </TimelineEvent>
+            <TimelineEvent gridRow={3}>
+                <HeadingTwo>Some text</HeadingTwo>
+            </TimelineEvent>
+            <TimelineEvent right mtop={'3rem'} gridRow={3}>
+                <HeadingTwo>Some text</HeadingTwo>
+            </TimelineEvent>
         </section>
+    )
+}
+
+
+function TimelineEvent ({children,right,mtop,gridRow}) {
+    
+    const inlineStyles = {
+        gridRow,
+        marginTop:mtop
+    }
+
+    const theStyles = [styles.event];
+    right && theStyles.push(styles.eventRight);
+
+    return (
+        <div className={theStyles.join(' ')} style={inlineStyles}>
+            {children}
+        </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import React from "react"
 import Layout from "../Components/layout"
 import Header from "../Components/Header/header"
 import AboutMe from '../Components/AboutMe/aboutMe'
+import Education from '../Components/Education/Education'
 
 export default function Home() {
   return (
     <Layout>
       <Header />
       <AboutMe/>
+      <Education/>
     </Layout>
   )
 }
