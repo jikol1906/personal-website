e3c6f5da0d7d2b4ff49abd65275827453e861113
borisgrunwald <borisgrunwald@gmail.com>
2020-07-12 13:36:59 +0200

    created basic timeline

---
+++ src/Components/Education/education.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './education.module.scss'
+
+export default function Education() {
+    return (
+        <section className={styles.education}>
+            <div className={styles.timeline}></div>
+        </section>
+    )
+}
