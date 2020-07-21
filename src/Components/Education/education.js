1eba50d193877fc05da85a05057319830d62f63f
borisgrunwald <borisgrunwald@gmail.com>
2020-07-21 11:37:19 +0200

    row offset added

--- src/Components/Education/education.js
+++ src/Components/Education/education.js
@@ -16,59 +16,65 @@ import Img from "gatsby-image"
 import { removeUneven, removeEven } from "../../Utils/Helperfunctions"
 
 export default function Education() {
-  const timeLineRef = useRef()
-  const timeLineIsInView = useMeasure(timeLineRef, 400)
-  const matches = useMediaQuery("(max-width: 50em)")
+  // const timeLineRef = useRef()
+  // const timeLineIsInView = useMeasure(timeLineRef, 400)
+  // const matches = useMediaQuery("(max-width: 50em)")
 
-  const timeLineEvents = (
-    <>
-      <div
-        style={!timeLineIsInView ? fadeInLeftCornerExited : fadeInEntered}
-        className={styles.column}
-      >
-        {!matches
-          ? timeEv
-              .filter(removeUneven)
-              .map(ev => (
-                <TimelineEvent
-                  key={ev.title}
-                  title={ev.title}
-                  text={ev.text}
-                  img={ev.img}
-                />
-              ))
-          : timeEv.map(ev => (
-              <TimelineEvent
-                key={ev.title}
-                title={ev.title}
-                text={ev.text}
-                img={ev.img}
-              />
-            ))}
-      </div>
-      {!matches && (
-        <div
-          className={`${styles.column} ${styles.columnRight}`}
-          style={!timeLineIsInView ? fadeInRightCornerExited : fadeInEntered}
-        >
-          {timeEv.filter(removeEven).map(ev => (
-            <TimelineEvent
-              right
-              key={ev.title}
-              title={ev.title}
-              text={ev.text}
-              img={ev.img}
-            />
-          ))}
-        </div>
-      )}
-    </>
-  )
+  // const timeLineEvents = (
+  //   <>
+  //     <div
+  //       style={!timeLineIsInView ? fadeInLeftCornerExited : fadeInEntered}
+  //       className={styles.column}
+  //     >
+  //       {!matches
+  //         ? timeEv
+  //             .filter(removeUneven)
+  //             .map(ev => (
+  //               <TimelineEvent
+  //                 key={ev.title}
+  //                 title={ev.title}
+  //                 text={ev.text}
+  //                 img={ev.img}
+  //               />
+  //             ))
+  //         : timeEv.map(ev => (
+  //             <TimelineEvent
+  //               key={ev.title}
+  //               title={ev.title}
+  //               text={ev.text}
+  //               img={ev.img}
+  //             />
+  //           ))}
+  //     </div>
+  //     {!matches && (
+  //       <div
+  //         className={`${styles.column} ${styles.columnRight}`}
+  //         style={!timeLineIsInView ? fadeInRightCornerExited : fadeInEntered}
+  //       >
+  //         {timeEv.filter(removeEven).map(ev => (
+  //           <TimelineEvent
+  //             right
+  //             key={ev.title}
+  //             title={ev.title}
+  //             text={ev.text}
+  //             img={ev.img}
+  //           />
+  //         ))}
+  //       </div>
+  //     )}
+  //   </>
+  // )
   return (
     <>
     <div className={styles.background}></div>
     <section id="educations" className={styles.education}>
-      <HeadingTwo otherStyles={styles.heading}>My Education</HeadingTwo>
+      <div className={`${styles.timeline} ${styles.timelineLeftBottom}`}></div>
+      <div className={`${styles.timeline} ${styles.timelineTopRightBottom}`}></div>
+      <div className={`${styles.timeline} ${styles.timelineTopLeftBottom}`}></div>
+      <div className={`${styles.timeline} ${styles.timelineTopRightBottom}`}></div>
+      <div className={styles.event}></div>
+      
+      {/* <HeadingTwo otherStyles={styles.heading}>My Education</HeadingTwo>
       <div
         style={
           !timeLineIsInView ? expandFromTopExited : expandFromTopEnteredDelay
@@ -76,54 +82,54 @@ export default function Education() {
         ref={timeLineRef}
         className={styles.timeline}
       ></div>
-      {timeLineEvents}
+      {timeLineEvents} */}
     </section>
     </>
   )
 }
 
-function TimelineEvent({ children, right, text, title, img }) {
-  const data = useStaticQuery(graphql`
-    query education {
-      allImage: allFile(filter: { relativeDirectory: { eq: "Education" } }) {
-        nodes {
-          base
-          childImageSharp {
-            fixed(height:70){
-              ...GatsbyImageSharpFixed
-            }
-          }
-        }
-      }
-    }
-  `)
+// function TimelineEvent({ children, right, text, title, img }) {
+//   const data = useStaticQuery(graphql`
+//     query education {
+//       allImage: allFile(filter: { relativeDirectory: { eq: "Education" } }) {
+//         nodes {
+//           base
+//           childImageSharp {
+//             fixed(height:70){
+//               ...GatsbyImageSharpFixed
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `)
 
-  const theStyles = [styles.event]
-  right && theStyles.push(styles.eventRight)
+//   const theStyles = [styles.event]
+//   right && theStyles.push(styles.eventRight)
 
-  if (img) {
-    const theImg = (
-      <Img
-        className={styles.img}
-        fixed={
-          data.allImage.nodes.find(i => i.base === img).childImageSharp.fixed
-        }
-      />
-    )
+//   if (img) {
+//     const theImg = (
+//       <Img
+//         className={styles.img}
+//         fixed={
+//           data.allImage.nodes.find(i => i.base === img).childImageSharp.fixed
+//         }
+//       />
+//     )
 
-    return (
-      <div className={theStyles.join(" ")}>
-        <HeadingTwo underlined>{title}</HeadingTwo>
-        <p>{text}</p>
-        {theImg}
-      </div>
-    )
-  } else {
-    return (
-      <div className={theStyles.join(" ")}>
-        <HeadingTwo underlined>{title}</HeadingTwo>
-        <p>{text}</p>
-      </div>
-    )
-  }
-}
+//     return (
+//       <div className={theStyles.join(" ")}>
+//         <HeadingTwo underlined>{title}</HeadingTwo>
+//         <p>{text}</p>
+//         {theImg}
+//       </div>
+//     )
+//   } else {
+//     return (
+//       <div className={theStyles.join(" ")}>
+//         <HeadingTwo underlined>{title}</HeadingTwo>
+//         <p>{text}</p>
+//       </div>
+//     )
+//   }
+// }
