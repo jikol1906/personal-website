7b55d1ec2a9d161375662a0cf0dfd14f0c6e57b4
borisgrunwald <borisgrunwald@gmail.com>
2020-07-22 11:44:55 +0200

    WIP

--- src/Components/Education/education.js
+++ src/Components/Education/education.js
@@ -15,22 +15,92 @@ import timeEv from "../../../static/TimelineEvents.json"
 import Img from "gatsby-image"
 import { removeUneven, removeEven } from "../../Utils/Helperfunctions"
 
+const timeLineEvents = [
+  {
+    title: "DTU",
+    text: "I have studied Software Engineering at DTU since 2018",
+    img:"dtu.png"
+  },
+  {
+    title: "KEA",
+    text: "I studied Computer Science at KEA from 2016 - 2018",
+    img:"kea.png"
+  },
+  {
+    title: "KVUC",
+    text: "Supplementary courses (Math)",
+    img:"kvuc.png"
+  },
+  {
+    title: "High School",
+    text: "Supplementary courses (Math)",
+    img:"kvuc.png"
+  },
+
+
+
+
+]
+
 export default function Education() {
   // const timeLineRef = useRef()
   // const timeLineIsInView = useMeasure(timeLineRef, 400)
   // const matches = useMediaQuery("(max-width: 50em)")
+
+  const timeLine = []
+
   return (
     <>
-    <div className={styles.background}></div>
-    <section id="educations" className={styles.education}>
-      <div className={`${styles.timeline} ${styles.timelineLeftBottom}`}></div>
-      <div className={`${styles.timeline} ${styles.timelineTopRightBottom}`}></div>
-      <div className={`${styles.timeline} ${styles.timelineTopLeftBottom}`}></div>
-      <div className={`${styles.timeline} ${styles.timelineTopRightBottom}`}></div>
-      <div className={styles.event} style={{gridRow:'3/6'}}><HeadingTwo black>DTU (Technical Univeristy Denmark)</HeadingTwo></div>
-      <div className={`${styles.event}`} style={{gridRow:'7/10'}}><HeadingTwo black>KEA</HeadingTwo></div>
-      <div className={`${styles.event}`} style={{gridRow:'11/14'}}><HeadingTwo black>High School</HeadingTwo></div>
-    </section>
+      <div className={styles.background}></div>
+      <section id="educations" className={styles.education}>
+        <div className={`${styles.timeline} ${styles.timelineStart}`}></div>
+        <div
+          style={{ gridRow: "2 / 7" }}
+          className={`${styles.timeline} ${styles.timelineRight}`}
+        ></div>
+        <div
+          style={{ gridRow: "6 / 11" }}
+          className={`${styles.timelineLeft}`}
+        ></div>
+        <div
+          style={{ gridRow: "10 / 15" }}
+          className={`${styles.timelineRight}`}
+        ></div>
+        <div
+          style={{ gridRow: "14 / 19" }}
+          className={`${styles.timelineLeft}`}
+        ></div>
+        <div
+          style={{ gridRow: "18 / 23" }}
+          className={`${styles.timelineRight}`}
+        ></div>
+        <div
+          style={{ gridRow: "22 / 27" }}
+          className={`${styles.timelineLeft}`}
+        ></div>
+        <div
+          style={{ gridRow: "26 / 31" }}
+          className={`${styles.timelineRight}`}
+        ></div>
+        <div className={styles.event} style={{ gridRow: "3/6" }}>
+          <HeadingTwo black>DTU (Technical Univeristy Denmark)</HeadingTwo>
+        </div>
+        <div
+          className={`${styles.event} ${styles.eventRight}`}
+          style={{ gridRow: "7/10" }}
+        >
+          <HeadingTwo black>KEA</HeadingTwo>
+        </div>
+        <div className={styles.event} style={{ gridRow: "11/14" }}>
+          <HeadingTwo black>High School</HeadingTwo>
+        </div>
+        <div
+          className={`${styles.event} ${styles.eventRight}`}
+          style={{ gridRow: "14/18" }}
+        >
+          <HeadingTwo black>High School</HeadingTwo>
+        </div>
+      </section>
     </>
   )
 }
