37ca2d1a7f68078794f5612074f550ce764b68e8
borisgrunwald <borisgrunwald@gmail.com>
2020-10-08 10:12:14 +0200

    changed nav 100vh to 100%

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,5 +1,5 @@
 import { createGlobalStyle } from "styled-components"
-import {mq, globalVariables } from "./variables"
+import { mq, globalVariables } from "./variables"
 
 const GlobalStyle = createGlobalStyle`
     ${globalVariables}
@@ -7,30 +7,28 @@ const GlobalStyle = createGlobalStyle`
     *,
     *::before,
     *::after {
-    margin: 0;
-    padding: 0;
-    box-sizing: inherit;
-
+        margin: 0;
+        padding: 0;
+        box-sizing: inherit;
     }
 
     html {
-    box-sizing: border-box;
-    font-size: 62.5%;
+        box-sizing: border-box;
+        font-size: 62.5%;
 
-    ${mq.large} {
-        font-size:50%;
-    }
+        ${mq.large} {
+            font-size:50%;
+        }
 
-    ${mq.smallest} {
-        font-size:45%;
-    }
-
+        ${mq.smallest} {
+            font-size:45%;
+        }
     }
 
     body {
-    font-family: 'Quicksand', sans-serif;
-    font-weight: 400;
-    background: var(--color-secondary);
+        font-family: 'Quicksand', sans-serif;
+        font-weight: 400;
+        background: var(--color-secondary);
     }
 `
 export default GlobalStyle
